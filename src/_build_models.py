15d70933eab7ecf5edd7b447a032630bde7b0eb4
anccduarte <antonionuno.123@gmail.com>
2023-05-22 18:21:44 +0100

    Added feature scaling

--- src/_build_models.py
+++ src/_build_models.py
@@ -1,11 +1,17 @@
 # -*- coding: utf-8 -*-
 
 import joblib
+import os
 import pandas as pd
 from pathlib import Path
 from sklearn.ensemble import HistGradientBoostingClassifier as HGBR
 from sklearn.metrics import accuracy_score, precision_score, recall_score
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+
+# create directory (if not already created)
+MODELS = "../models"
+Path(MODELS).mkdir(exist_ok=True)
 
 """
 AS OF NOW, THE FINAL MODEL IS ONLY TRAINED ON 75% OF THE DATA. THE REMAINING
@@ -34,9 +40,16 @@ def build_model_and_display_stats(file_name: str) -> None:
                                                         y,
                                                         test_size=0.25,
                                                         random_state=42)
+    # scale data (X_train and X_test) according to X_train
+    scaler = MinMaxScaler().fit(X_train)
+    X_train, X_test = scaler.transform(X_train), scaler.transform(X_test)
+    # save scaler in .joblib file to be used when predicting (if inexistent)
+    scaler_name = f"{MODELS}/{file_name}_scaler.joblib"
+    if not os.path.exists(scaler_name):
+        joblib.dump(scaler, scaler_name)
     # build model at save it
     model = HGBR().fit(X_train, y_train)
-    joblib.dump(model, f"../models/{file_name}.joblib")
+    joblib.dump(model, f"{MODELS}/{file_name}.joblib")
     # print statistics (accuracy, precision and recall)
     y_pred = model.predict(X_test)
     accuracy = accuracy_score(y_test, y_pred)
--- src/predict_function.py
+++ src/predict_function.py
@@ -13,7 +13,7 @@ class PredictFunction:
     contained in the .fasta file pointed to by <path>.
     """
     
-    def __init__(self, path: str) -> None:
+    def __init__(self, path: str, ttable: str, icodons: tuple) -> None:
         """
         Initializes an instance of PredictFunction.
         
@@ -21,6 +21,10 @@ class PredictFunction:
         ----------
         path: str
             The path to the .fasta file
+        ttable: str
+            The identifier of the translation table to be used
+        icodons: tuple
+            A tuple of possible initiation codons given the table <ttable>
             
         Attributes
         ----------
@@ -28,6 +32,8 @@ class PredictFunction:
             A list of ML models
         """
         self.path = path
+        self.ttable = ttable
+        self.icodons = icodons
         self._models = PredictFunction._load_models()
         
     def __repr__(self) -> str:
@@ -60,9 +66,11 @@ class PredictFunction:
         Constructs and returns a featurized dataset from the sequences contained in
         the file pointed to by <path>.
         """
-        data = MLDataset(file=self.path, protein_name="unknown")
-        X = data.build_dataset().iloc[:, :-1]
-        return X
+        data = MLDataset(file=self.path,
+                         prot_name="unknown",
+                         ttable=self.ttable,
+                         icodons=self.icodons).build_dataset()
+        return data.iloc[:, :-1]
     
     def _get_descriptions(self) -> list:
         """
@@ -86,26 +94,42 @@ class PredictFunction:
         """
         # get featurized dataset
         X = self._get_dataset()
+        # load scaler used to scale features in "all.csv"
+        scaler_all = joblib.load("../models/all_scaler.joblib")
         # get models
         ALL, MOD, REP, LYSIS, LYS_REP, PACK, STRUCT, OTHER = self._models
         # predict functional class
-        preds_func_class = ALL.predict(X)
-        # predict function
+        preds_func_class = ALL.predict(scaler_all.transform(X))
+        # initialize empty list to store predicted functions
         preds_func = []
-        # iterate through rows of <X>
+        # iterate through rows of <X> and predict function
         for i, vec in X.iterrows():
-            # get appropriate model
+            # get appropriate model and load scaler
             func_class = preds_func_class[i]
-            if func_class == "dna-modification": model = MOD
-            elif func_class == "dna-replication": model = REP
-            elif func_class == "lysis": model = LYSIS
-            elif func_class == "lysogeny-repressor": model = LYS_REP
-            elif func_class == "packaging": model = PACK
-            elif func_class == "structural": model = STRUCT
-            elif func_class == "other": model = OTHER
+            if func_class == "dna-modification":
+                model = MOD
+                scaler = joblib.load("../models/dna_modification_scaler.joblib")
+            elif func_class == "dna-replication":
+                model = REP
+                scaler = joblib.load("../models/dna_replication_scaler.joblib")
+            elif func_class == "lysis":
+                model = LYSIS
+                scaler = joblib.load("../models/lysis_scaler.joblib")
+            elif func_class == "lysogeny-repressor":
+                model = LYS_REP
+                scaler = joblib.load("../models/lysogeny_repressor_scaler.joblib")
+            elif func_class == "packaging":
+                model = PACK
+                scaler = joblib.load("../models/packaging_scaler.joblib")
+            elif func_class == "structural":
+                model = STRUCT
+                scaler = joblib.load("../models/structural_scaler.joblib")
+            elif func_class == "other":
+                model = OTHER
+                scaler = joblib.load("../models/other_scaler.joblib")
             # predict and save
             vec = np.array(vec).reshape(1,-1)
-            preds_func.append(model.predict(vec)[0])
+            preds_func.append(model.predict(scaler.transform(vec))[0])
         # return predictions
         return preds_func_class, preds_func
 
