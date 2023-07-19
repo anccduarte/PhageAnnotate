6e239c6c3ae83daa3e60c5c8f6be43cb111e6d69
anccduarte <antonionuno.123@gmail.com>
2023-07-19 18:31:55 +0100

    Minor changes ('models_dir' is now a command line argument)

--- src/_predict.py
+++ src/_predict.py
@@ -10,22 +10,26 @@ if __name__ == "__main__":
     Path("../results").mkdir(exist_ok=True)
     
     # read arguments (sys.argv)
-    args = utils.get_args(("-path",))
+    args = utils.get_args(("-path",),
+                          ("-models_dir",))
     path = args.path
+    models_dir = args.models_dir
+    
+    # check whether <path> and <models_dir> were provided to sys.argv
+    if path is None or models_dir is None:
+        e = ("'path' and 'models_dir' have no default values. Please, do:\n"
+             ">>> python gene_products.py -path <path> -models_dir <models_dir>")
+        raise Exception(e)
     
     # remaining arguments
     ttable = "11"
     icodons = ("TTG", "CTG", "ATT", "ATC", "ATA", "ATG", "GTG")
-    
-    # check whether <path> was provided to sys.argv
-    if path is None:
-        raise Exception("'path' has no default value. Please, do:\n"
-                        ">>> python gene_products.py -path <path>")
     
     # get name of the file
     name = path.split("/")[-1].split(".")[0]
     
     # get predictions
     PredictFunction(path=path,
+                    models_dir=models_dir,
                     ttable=ttable,
                     icodons=icodons).save_results(name=name)
