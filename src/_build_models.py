92c7ff22771195bcad9ad900f8e119db4fda28c7
anccduarte <antonionuno.123@gmail.com>
2023-07-26 15:57:43 +0100

    Refactored code (model construction performed via MLModel)

--- src/_build_models.py
+++ src/_build_models.py
@@ -1,190 +1,107 @@
 # -*- coding: utf-8 -*-
 
-import joblib
-import numpy as np
-import os
-import pandas as pd
-from sklearn.base import BaseEstimator
-from sklearn.ensemble import RandomForestClassifier as RFC
-from sklearn.feature_selection import SelectFromModel
-from sklearn.metrics import accuracy_score, precision_score, recall_score
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler
-
-"""
-IMPORTANT
----
-The module must be executed in the command line. Importing individual functions
-will throw an error, as all of them include global variables which are only
-accessible to this particular module. These variables are: "DATABASE" and
-"MODELS".
-"""
-
-def get_scaler(dataset: str, X_train: np.ndarray) -> MinMaxScaler:
-    """
-    If the process of feature scaling was not previously performed for the
-    dataset <dataset>, the scaler is computed. Otherwise, the scaler is loaded
-    from a .joblib file. Returns the scaler object.
-    
-    Parameters
-    ----------
-    dataset: str
-        The name of the dataset
-    X_train: np.ndarray
-        The scaled training data
-    """
-    scaler_name = f"{MODELS}/{dataset}_scaler.joblib"
-    if not os.path.exists(scaler_name):
-        scaler = MinMaxScaler().fit(X_train)
-        # only saved for use in this module (saves some time)
-        joblib.dump(scaler, scaler_name)
-    else:
-        scaler = joblib.load(scaler_name)
-    return scaler
-
-def get_support_vector(dataset: str,
-                       X_train: np.ndarray,
-                       y_train: np.ndarray) -> np.ndarray:
-    """
-    If the process of feature selection was not previously performed for the
-    dataset <dataset>, the features are selected and the respective support
-    vector is computed. Otherwise, the support vector is loaded from a .joblib
-    file. Returns the support vector.
-    
-    Parameters
-    ----------
-    dataset: str
-        The name of the dataset
-    X_train: np.ndarray
-        The scaled training data
-    y_train: np.ndarray
-        The label vector (response variable)
-    """
-    support_name = f"{MODELS}/{dataset}_support.joblib"
-    if not os.path.exists(support_name):
-        selector = SelectFromModel(estimator=RFC()).fit(X_train, y_train)
-        support = selector.get_support()
-        # only saved for use in this module (saves a lot of time)
-        joblib.dump(support, support_name)
-    else:
-        support = joblib.load(support_name)
-    return support
-
-def build_model_and_display_stats(dataset: str, model: BaseEstimator) -> None:
-    """
-    Builds an ML model fed on the data present in '<DATABASE>/<file_name>.csv',
-    saves it to a .joblib file (TEMPORARY) and prints some useful statisitics
-    on the testing data (accuracy, precision and recall).
-    
-    Parameters
-    ----------
-    dataset: str
-        The name of the .csv file containing the data
-    """
-    # ---
-    # NOTE: THIS WILL EXCLUSIVELY BE USED TO TRAIN A MODEL WITH 80% OF THE DATA
-    # AND TEST ON THE REMAINING 20% (HENCE, THE MODELS, SCALERS AND SEELCTORS
-    # WILL NOT BE SAVED HERE...). THE ONLY PURPOSE OF THIS FUNCTION IS TO TRAIN
-    # AN ML MODEL, TEST IT AND DISPLAY THE RESULTS. THIS INCLUDES: SPLIT THE
-    # DATA INTO TRAIN AND TEST, SCALE THE DATA BASED ON THE "CHARACTERISTICS"
-    # OF THE TRAINING DATA, SELECT FEATURES (ALSO, BASED ON THE TRAINING DATA),
-    # TRAIN THE MODEL, TEST THE MODEL, AND DISPLAY THE RESULTS OF TESTING. ONLY
-    # THE BEST MODELS (ML ALGORITHM + DATASET) WILL BE FULLY TRAINED IN A
-    # SEPARATE FUNCTION / CLASS (ONLY THEN THE MODELS, SCALERS AND SEELCTORS
-    # WILL BE SAVED). AS OF NOW, THE MODELS AND SCALERS ARE SAVED HERE FOR
-    # PRACTICALITY REASONS.
-    # ---
-    # read data
-    print(f"{dataset.upper()} -> {model().__class__.__name__}")
-    print("Reading and splitting data...")
-    data = pd.read_csv(f"{DATABASE}/{dataset}.csv")
-    # ---
-    # split features (explanatory) and label (response)
-    X, y = data.iloc[:, 1:-1], data.iloc[:, -1]
-    # ---
-    # train-test split ("stratify=y" keeps class proportions)
-    X_train, X_test, y_train, y_test = train_test_split(X,
-                                                        y,
-                                                        stratify=y,
-                                                        test_size=0.20,
-                                                        random_state=0)
-    # ---
-    # scale data (X_train and X_test) according to X_train
-    print("Scaling data...")
-    scaler = get_scaler(dataset, X_train)
-    X_train, X_test = scaler.transform(X_train), scaler.transform(X_test)
-    # ---
-    # select features (based on the Random Forest Classifier...)
-    print("Selecting features...")
-    support = get_support_vector(dataset, X_train, y_train)
-    X_train, X_test = X_train[:, support], X_test[:, support]
-    # ---
-    # build model
-    print("Building model on 80% of the data...")
-    estimator = model().fit(X_train, y_train)
-    # ---
-    # TEMPORARY -> save model (only for practicality reasons)
-    joblib.dump(estimator, f"{MODELS}/{dataset}.joblib")
-    # ---
-    # print statistics (accuracy, precision and recall)
-    print("Testing model on 20% of the data...")
-    y_pred = estimator.predict(X_test)
-    accuracy = accuracy_score(y_test, y_pred)
-    precision = precision_score(y_test, y_pred, average="macro")
-    recall = recall_score(y_test, y_pred, average="macro")
-    print(f"Metrics on testing data ({dataset!r}):\n"
-          f"- {accuracy = :.2%}\n- {precision = :.2%}\n- {recall = :.2%}")
-    
-    
 if __name__ == "__main__":
     
-    import utils
-    from pathlib import Path
-    from sklearn.ensemble import HistGradientBoostingClassifier as HGBC
+    # command line arguments
     # ---
-    # from sklearn.naive_bayes import CategoricalNB as NaiveBayes
-    # from sklearn.neighbors import KNeighborsClassifier as KNC
-    # from sklearn.neural_network import MLPClassifier as MLPC
-    # from sklearn.svm import SVC
-    # from sklearn.tree import DecisionTreeClassifier as DTC
-    # ---
+    # - final_model: -final_model (options: "yes" or "no")
+    # - init: -init (options: "yes" or "no")
     
-    # get command line argument
-    args = utils.get_args(("-init",))
+    # deduced from command line arguments
+    # ---
+    # - models_dir: deduced from <init> ("../models" or "../models_cs")
+    # - db_name: deduced from <init> ("../database" or "../database_cs")
+    # - test_size: deduced from <final_model> (0.1 or 0.2)
+    # - behavior of the loop datasets/algorithms determined by <final_model>
+    
+    import utils
+    from ml_model import MLModel
+    from pathlib import Path
+    
+    # get command line arguments
+    # ---
+    args = utils.get_args(("-final_model",),
+                          ("-init",))
+    final_model = args.final_model
     init = args.init
     
-    # verify validity of <init> (1)
-    if init is None:
-        raise Exception("<init> has no default value. Please, do:\n"
-                        ">>> python _build_db.py -init <init>")
-    
-    # verify validity of <init> (2)
+    # check validity of command line arguments (1)
+    # ---
+    if final_model is None or init is None:
+        raise Exception("'final_model' and 'init' have no default values. Please do:\n"
+                        ">>> python -final_model <final_model> -init <init>")
+        
+    # check validity of <final_model> (2)
+    # ---
+    if final_model not in {"yes", "no"}:
+        raise ValueError(f"{final_model!r} is not valid for 'final_model'."
+                         "Choose one of {{'yes', 'no'}}.")
+        
+    # check validity of <init> (3)
+    # ---
     if init not in {"yes", "no"}:
         raise ValueError(f"{init!r} is not valid for 'init'."
                          "Choose one of {{'yes', 'no'}}.")
-    
-    # initialize global variables
-    DATABASE = "../database" if init == "yes" else "../database_cs"
-    MODELS = "../models" if init == "yes" else "../models_cs"
-    
-    # create directory (if not already created)
-    Path(MODELS).mkdir(exist_ok=True)
-    
-    # tuple of datasets that will feed the ML models
-    datasets = ("all",
-                "dna_modification",
-                "dna_replication",
-                "lysis",
-                "lysogeny_repressor",
-                "packaging",
-                "structural")
-    
-    # tuple of ML algorithms used to build the models
-    algorithms = (HGBC,) # (HGBC, NaiveBayes, KNC, DTC, RF, SVC, MLPC)
-    
-    print("---")
-    for dataset in datasets:
-        for algo in algorithms:
-            build_model_and_display_stats(dataset=dataset, model=algo)
-            print("---")
         
+    # reassign <final_model> and <init>
+    # ---
+    final_model = True if final_model == "yes" else False
+    init = True if init == "yes" else False
+    
+    # initialize <models_dir>
+    # ---
+    models_dir = "../models" if init else "../models_cs"
+    Path(models_dir).mkdir(exist_ok=True)
+    
+    # initialize <db_name> and <test_size>
+    # ---
+    db_name = "database" if init else "database_cs"
+    test_size = 0.1 if final_model else 0.2
+        
+    # initialize <datasets>
+    # ---
+    datasets = ["all", "dna_modification", "dna_replication", "lysis",
+                "lysogeny_repressor", "packaging", "structural"]
+    
+    # initilaize <algorithms>
+    # ---
+    algorithms = ["gradient-boosting", "random-forest", "naive-bayes",
+                  "k-nearest-neighbors", "neural-network", "support-vector-machine",
+                  "decision-tree"]
+    
+    # logic for model construction
+    # ---
+    # - if <final_model> is set to True, the combinations dataset-algorithm are
+    #   defined -> ask for list of algorithms that match each dataset and zip the
+    #   datasets and the algorithms
+    # - if <final_model> is set to False, initialize list with all algorithms and
+    #   test all combinations dataset-algorithm
+    # ---
+    if final_model:
+        # get list of algorithms
+        print(f"\nType the list of ML algorithms matching the datasets:\n{datasets}")
+        algorithms_in = eval(input("Algorithms: "))
+        # check validity of the input
+        if any(algo not in algorithms for algo in algorithms_in):
+            raise ValueError("The list of algorithms is not valid. Please choose "
+                             f"one of the following:\n{{{', '.join(algorithms)}}}")
+        # zip datasets and algorithms and train final models
+        for data, algo in zip(datasets, algorithms_in):
+            MLModel(data_path=f"../{db_name}/{data}.csv",
+                    models_dir=models_dir,
+                    algorithm=algo,
+                    test_size=test_size,
+                    init=init,
+                    final_model=final_model).build_model()
+            print("------------")
+    else:
+        # test all combinations dataset-algorithm
+        for data in datasets:
+            for algo in algorithms:
+                MLModel(data_path=f"../{db_name}/{data}.csv",
+                        models_dir=models_dir,
+                        algorithm=algo,
+                        test_size=test_size,
+                        init=init,
+                        final_model=final_model).build_model()
+                print("------------")
